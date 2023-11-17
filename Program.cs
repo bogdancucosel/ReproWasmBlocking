b00f7c0408e36bbd00ba1046653d5186e3477ae1
bogdancucosel <bogdan.cucosel@outlook.com>
2023-11-17 11:53:05 +0200

    adding repro

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Runtime.InteropServices.JavaScript;
+using System.Threading.Tasks;
+
+Console.WriteLine("Hello, Browser!");
+
+public partial class MyClass
+{
+    [JSExport]
+    internal static string Greeting()
+    {
+        var text = $"Hello, World! Greetings from {GetHRef()}";
+        Console.WriteLine(text);
+        return text;
+    }
+
+    [JSExport]
+    public static async Task CreateFileSyncReadAsync(string path)
+    {
+        var text = "The text written to the file";
+        File.WriteAllText(path, text);
+        Console.WriteLine($"Sync written to file {path}: {text}");
+
+        var textReadSync = File.ReadAllText(path);
+        Console.WriteLine($"Sync read file {path}: {textReadSync}");
+
+        Console.WriteLine("Will start to read async the file; this will block when called from onclick of the button...");
+        var textReadAsync = await File.ReadAllTextAsync(path);
+        Console.WriteLine($"Async read file {path}: {textReadAsync}");
+    }
+
+    [JSImport("window.location.href", "main.js")]
+    internal static partial string GetHRef();
+}
---
+++ wwwroot/main.js
@@ -0,0 +1,29 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+import { dotnet } from './_framework/dotnet.js'
+
+const { setModuleImports, getAssemblyExports, getConfig } = await dotnet
+    .withDiagnosticTracing(false)
+    .withApplicationArgumentsFromQuery()
+    .create();
+
+setModuleImports('main.js', {
+    window: {
+        location: {
+            href: () => globalThis.window.location.href
+        }
+    }
+});
+
+const config = getConfig();
+const exports = await getAssemblyExports(config.mainAssemblyName);
+const text = exports.MyClass.Greeting();
+console.log(text);
+await exports.MyClass.CreateFileSyncReadAsync("/testFromMain.txt");
+
+document.getElementById('out').innerHTML = text;
+document.getElementById('btn1').onclick = async () => {
+    await exports.MyClass.CreateFileSyncReadAsync("/testFromBtn1.txt");
+};
+await dotnet.run();
